65a215a495532ea9c0d3d4a8e1203cc72eefa8b9
mariana10rios <140272078+mariana10rios@users.noreply.github.com>
2023-08-31 02:14:49 +0000

    completed

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".us", ".io"];
+  let domainList = "";
+  pronoun.forEach(function(pnoun) {
+    adj.forEach(function(adjective) {
+      noun.forEach(function(Noun) {
+        extensions.forEach(function(ext) {
+          domainList = `${domainList}${pnoun}${adjective}${Noun}${ext} <br>`;
+        });
+      });
+    });
+  });
+  document.getElementById("domains").innerHTML = domainList;
 };
